e2fe6d3d414f6f8b5ddb5111a844a45b24f0555d
Mark <gitcommits@mkwsystems.com>
2021-03-02 10:59:03 -0800

    Both journal pages now have the templated navbar

--- routes/journal.js
+++ routes/journal.js
@@ -4,5 +4,31 @@
  */
 
 exports.view = function(req, res){
-  res.render('journal'); // journal here refers to views/journal.handlebars
+  var info = {};
+  info.navbarButtons = [
+    {
+      "aTag": "<a href=\"home\">",
+      "buttonTag": "<button class=\"navButton\">",
+      "icon": "<i class=\"fas fa-plus fa-3x\"></i>",
+      "label": "Add Emotion"
+    },
+    {
+      "aTag": "<a href=\"calendarMonth\">",
+      "buttonTag": "<button class=\"navButton\">",
+      "icon": "<i class=\"far fa-calendar-alt fa-3x\"></i>",
+      "label": "Calendar"
+    },
+    {
+      "aTag": "<a class=\"active\">",
+      "buttonTag": "<button class=\"navButton\" disabled>",
+      "icon": "<i class=\"far fa-file-alt fa-3x\"></i>",
+      "label": "Journal"
+    },
+    {
+      "aTag": "<a href=\"settings\">",
+      "buttonTag": "<button class=\"navButton\">",
+      "icon": "<i class=\"fas fa-cog fa-3x\"></i>",
+      "label": "Settings"
+    }];
+  res.render('journal', info); // journal here refers to views/journal.handlebars
 };
