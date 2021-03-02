1219f035b95579f39cc10a85be9e53ba0165c5e4
Mark <gitcommits@mkwsystems.com>
2021-03-02 10:52:14 -0800

    Each emotion page now has the templated navbar

--- routes/enjoyment.js
+++ routes/enjoyment.js
@@ -4,5 +4,31 @@
  */
 
 exports.view = function(req, res){
-  res.render('enjoyment'); // enjoyment here refers to views/enjoyment.handlebars
+  var info = {};
+  info.navbarButtons = [
+    {
+      "aTag": "<a class=\"active\">",
+      "buttonTag": "<button class=\"navButton\" id=\"addEmotion\" disabled>",
+      "icon": "<i class=\"fas fa-plus fa-3x\"></i>",
+      "label": "Add Emotion"
+    },
+    {
+      "aTag": "<a href=\"calendarMonth\">",
+      "buttonTag": "<button class=\"navButton\">",
+      "icon": "<i class=\"far fa-calendar-alt fa-3x\"></i>",
+      "label": "Calendar"
+    },
+    {
+      "aTag": "<a href=\"journal\">",
+      "buttonTag": "<button class=\"navButton\">",
+      "icon": "<i class=\"far fa-file-alt fa-3x\"></i>",
+      "label": "Journal"
+    },
+    {
+      "aTag": "<a href=\"settings\">",
+      "buttonTag": "<button class=\"navButton\">",
+      "icon": "<i class=\"fas fa-cog fa-3x\"></i>",
+      "label": "Settings"
+    }];
+  res.render('enjoyment', info); // enjoyment here refers to views/enjoyment.handlebars
 };
--- routes/sadness.js
+++ routes/sadness.js
@@ -4,5 +4,31 @@
  */
 
 exports.view = function(req, res){
-  res.render('sadness'); // sadness here refers to views/sadness.handlebars
+  var info = {};
+  info.navbarButtons = [
+    {
+      "aTag": "<a class=\"active\">",
+      "buttonTag": "<button class=\"navButton\" id=\"addEmotion\" disabled>",
+      "icon": "<i class=\"fas fa-plus fa-3x\"></i>",
+      "label": "Add Emotion"
+    },
+    {
+      "aTag": "<a href=\"calendarMonth\">",
+      "buttonTag": "<button class=\"navButton\">",
+      "icon": "<i class=\"far fa-calendar-alt fa-3x\"></i>",
+      "label": "Calendar"
+    },
+    {
+      "aTag": "<a href=\"journal\">",
+      "buttonTag": "<button class=\"navButton\">",
+      "icon": "<i class=\"far fa-file-alt fa-3x\"></i>",
+      "label": "Journal"
+    },
+    {
+      "aTag": "<a href=\"settings\">",
+      "buttonTag": "<button class=\"navButton\">",
+      "icon": "<i class=\"fas fa-cog fa-3x\"></i>",
+      "label": "Settings"
+    }];
+  res.render('sadness', info); // sadness here refers to views/sadness.handlebars
 };
