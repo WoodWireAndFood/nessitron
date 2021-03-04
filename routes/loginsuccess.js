b0e62871e0a3c52b03284d83369362f1d514ed24
Mark <gitcommits@mkwsystems.com>
2021-03-04 14:57:19 -0800

    Login now checks passwords

--- routes/loginsuccess.js
+++ routes/loginsuccess.js
@@ -3,7 +3,25 @@
  */
 
 exports.writeUser = function(req, res){
+    var jsonFilename = "users.json";
+    var info = JSON.parse('{"loginMessage": "Invalid username or password. Please try again."}');
     var username = req.query.uname;
-    res.cookie("username", username);
-    res.render('loginsuccess'); // loginsuccess here refers to views/loginsuccess.handlebars which is a straight redirect to home
+    var password = req.query.pword;
+    var fs = require('fs');
+    var raw = fs.readFileSync(jsonFilename, 'utf8');
+    var fileInfo = JSON.parse(raw);
+    //Check passwords match
+    if (typeof(fileInfo.users[username]) == 'undefined') {
+      console.log(username + " doesn't exist.");
+      res.render('login', info);
+    }
+    else if (fileInfo.users[username].password == password){
+      console.log("Successful login for " + username);
+      res.cookie("username", username);
+      res.render('loginsuccess'); // loginsuccess here refers to views/loginsuccess.handlebars which is a straight redirect to home
+    }
+    else {
+      console.log("Failed login for " + username);
+      res.render('login', info);
+    }
   }
