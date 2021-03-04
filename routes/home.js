411b817e981ba72bcd130257f1056ccb983c3d6f
Mark <gitcommits@mkwsystems.com>
2021-03-03 18:27:23 -0800

    Login now sets cookie. Home checks whether logging in and redirects to login if not. Login page now has a link to create account

--- routes/home.js
+++ routes/home.js
@@ -4,51 +4,59 @@
  */
 
 exports.view = function(req, res){
-  var fs = require('fs');
-  var raw = fs.readFileSync('user.json', 'utf8');
-  console.log(raw);
-  var info = JSON.parse(raw);
-  info.navbarButtons = [
-    {
-      "aTag": "<a class=\"active\">",
-      "buttonTag": "<button class=\"navButton\" id=\"addEmotion\" disabled>",
-      "icon": "<i class=\"fas fa-plus fa-3x\"></i>",
-      "label": "Add Emotion"
-    },
-    {
-      "aTag": "<a href=\"calendarMonth\">",
-      "buttonTag": "<button class=\"navButton\">",
-      "icon": "<i class=\"far fa-calendar-alt fa-3x\"></i>",
-      // "label": "Calendar"
-      "label": "Calendar"
-    },
-    {
-      "aTag": "<a href=\"journal\">",
-      "buttonTag": "<button class=\"navButton\">",
-      "icon": "<i class=\"far fa-file-alt fa-3x\"></i>",
-      "label": "Journal"
-    },
-    {
-      "aTag": "<a href=\"settings\">",
-      "buttonTag": "<button class=\"navButton\">",
-      "icon": "<i class=\"fas fa-cog fa-3x\"></i>",
-      "label": "Settings"
-    }];
-    info.emotionButtons = [
+  // res.cookie("username", "thisuser");
+  // var fs = require('fs');
+  // var raw = fs.readFileSync('user.json', 'utf8');
+  // console.log(raw);
+  // var info = JSON.parse(raw);
+  var username = req.cookies.username;
+  if (typeof(username) == 'undefined') {
+    res.render('loginRedirect');
+  }
+  else{
+    var info = JSON.parse('{"username": "' + username + '"}');
+    info.navbarButtons = [
       {
-        "emoButton": "<a href=\"enjoyment\" class=\"nostyle\"><button type=\"button\" class=\"list-group-item list-group-item-action btn-block emotion\" id=\"enjoyment\"><i class=\"far fa-smile-beam\"></i> Enjoyment</button></a>"
+        "aTag": "<a class=\"active\">",
+        "buttonTag": "<button class=\"navButton\" id=\"addEmotion\" disabled>",
+        "icon": "<i class=\"fas fa-plus fa-3x\"></i>",
+        "label": "Add Emotion"
       },
       {
-        "emoButton": "<a href=\"sadness\" class=\"nostyle\"><button type=\"button\" class=\"list-group-item list-group-item-action btn-block emotion\" id=\"sadness\"><i class=\"far fa-sad-tear\"></i> Sadness</button></a>"
+        "aTag": "<a href=\"calendarMonth\">",
+        "buttonTag": "<button class=\"navButton\">",
+        "icon": "<i class=\"far fa-calendar-alt fa-3x\"></i>",
+        // "label": "Calendar"
+        "label": "Calendar"
       },
       {
-        "emoButton": "<a href=\"fear\" class=\"nostyle\"><button type=\"button\" class=\"list-group-item list-group-item-action btn-block emotion\" id=\"fear\"><i class=\"far fa-flushed\"></i> Fear</button></a>"
+        "aTag": "<a href=\"journal\">",
+        "buttonTag": "<button class=\"navButton\">",
+        "icon": "<i class=\"far fa-file-alt fa-3x\"></i>",
+        "label": "Journal"
       },
       {
-        "emoButton": "<a href=\"anger\" class=\"nostyle\"><button type=\"button\" class=\"list-group-item list-group-item-action btn-block emotion\" id=\"anger\"><i class=\"far fa-angry\"></i> Anger</button></a>"
-      },
-      {
-        "emoButton": "<a href=\"disgust\" class=\"nostyle\"><button type=\"button\" class=\"list-group-item list-group-item-action btn-block emotion\" id=\"disgust\"><i class=\"far fa-grin-tongue-squint\"></i> Disgust</button></a>"
+        "aTag": "<a href=\"settings\">",
+        "buttonTag": "<button class=\"navButton\">",
+        "icon": "<i class=\"fas fa-cog fa-3x\"></i>",
+        "label": "Settings"
       }];
-  res.render('home', info); // home here refers to views/home.handlebars
+      info.emotionButtons = [
+        {
+          "emoButton": "<a href=\"enjoyment\" class=\"nostyle\"><button type=\"button\" class=\"list-group-item list-group-item-action btn-block emotion\" id=\"enjoyment\"><i class=\"far fa-smile-beam\"></i> Enjoyment</button></a>"
+        },
+        {
+          "emoButton": "<a href=\"sadness\" class=\"nostyle\"><button type=\"button\" class=\"list-group-item list-group-item-action btn-block emotion\" id=\"sadness\"><i class=\"far fa-sad-tear\"></i> Sadness</button></a>"
+        },
+        {
+          "emoButton": "<a href=\"fear\" class=\"nostyle\"><button type=\"button\" class=\"list-group-item list-group-item-action btn-block emotion\" id=\"fear\"><i class=\"far fa-flushed\"></i> Fear</button></a>"
+        },
+        {
+          "emoButton": "<a href=\"anger\" class=\"nostyle\"><button type=\"button\" class=\"list-group-item list-group-item-action btn-block emotion\" id=\"anger\"><i class=\"far fa-angry\"></i> Anger</button></a>"
+        },
+        {
+          "emoButton": "<a href=\"disgust\" class=\"nostyle\"><button type=\"button\" class=\"list-group-item list-group-item-action btn-block emotion\" id=\"disgust\"><i class=\"far fa-grin-tongue-squint\"></i> Disgust</button></a>"
+        }];
+    res.render('home', info); // home here refers to views/home.handlebars
+  }
 };
--- routes/loginsuccess.js
+++ routes/loginsuccess.js
@@ -4,10 +4,12 @@
 
 exports.writeUser = function(req, res){
     var username = req.query.uname;
-    var toWrite = '{"username": "' + username + '" }';
-    console.log(toWrite);
-    var fs = require('fs');
-    var info = JSON.parse(toWrite);
-    fs.writeFile("user.json", JSON.stringify(info), 'utf8', function (){});
+    res.cookie("username", username);
+    // var toWrite = '{"username": "' + username + '" }';
+    // console.log(toWrite);
+    // var fs = require('fs');
+    // var info = JSON.parse(toWrite);
+    // fs.writeFile("user.json", JSON.stringify(info), 'utf8', function (){});
+    // sessionStorage("username", username);
     res.render('loginsuccess'); // loginsuccess here refers to views/loginsuccess.handlebars which is a straight redirect to home
   }
